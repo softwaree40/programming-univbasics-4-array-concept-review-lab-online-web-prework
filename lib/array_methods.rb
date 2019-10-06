a61fbdbd4c6e42d0740d4ccfa1161582676fda14
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-06 00:32:31 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,11 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-    return max.array
+    (0..array).each do |num|
+      if num > array.length
+        return num
+      end
+    end
 end
 
 def find_min_value(array)
